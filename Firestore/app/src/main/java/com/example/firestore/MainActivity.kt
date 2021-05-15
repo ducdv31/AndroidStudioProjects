dcc66be9ab843b93cf2ae4dc2a05fdcf4f36fa10
Đặng Đức <39004625+DangDuc1999@users.noreply.github.com>
2021-05-15 15:07:27 +0700

    Ok set data

--- Firestore/app/src/main/java/com/example/firestore/MainActivity.kt
+++ Firestore/app/src/main/java/com/example/firestore/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.firestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("ID")
+            .document("Date")
+            .set(user)
+
+        db.collection("ID")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.e("TAG", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("TAG", "Error getting documents.", exception)
+            }
     }
 }
