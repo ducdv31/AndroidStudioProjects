722b328f6f9892e96b88f040a5d0dc8d85d96df1
Đặng Đức <39004625+DangDuc1999@users.noreply.github.com>
2021-06-10 02:57:43 +0700

    Retrofit post ok, get null

--- Retrofit/app/src/main/java/com/example/retrofit/MainActivity.kt
+++ Retrofit/app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.retrofit
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.retrofit.api.ApiServices
 import com.example.retrofit.model.Currentcy
+import com.example.retrofit.model.Post
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var usdvnd: TextView
     lateinit var callApi: TextView
 
+    lateinit var postResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,29 +29,58 @@ class MainActivity : AppCompatActivity() {
         source = findViewById(R.id.source)
         usdvnd = findViewById(R.id.usdvnd)
         callApi = findViewById(R.id.call_get_api)
+        postResult = findViewById(R.id.id_post_result)
 
         callApi.setOnClickListener {
             callApiGet()
+            startPost()
         }
     }
 
     private fun callApiGet() {
+        /* call api with map */
         //link api:  http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
-        ApiServices.apiServices.convertUusToVnd("843d4d34ae72b3882e3db642c51e28e6", "VND", "USD", 1)
+        val valueApi: MutableMap<String, String> = mutableMapOf()
+        valueApi["access_key"] = "843d4d34ae72b3882e3db642c51e28e6"
+        valueApi["currencies"] = "VND"
+        valueApi["source"] = "USD"
+        valueApi["format"] = "1"
+        Log.e("TAG", "callApiGet: $valueApi")
+        ApiServices.apiServices.convertUusToVndMap(valueApi)
             .enqueue(object : Callback<Currentcy> {
                 override fun onResponse(call: Call<Currentcy>, response: Response<Currentcy>) {
                     Toast.makeText(this@MainActivity, "Call Ok", Toast.LENGTH_SHORT).show()
                     val currentcy: Currentcy? = response.body()
                     currentcy?.let {
-                        if (currentcy.success){
+                        if (currentcy.success) {
                             term.text = currentcy.terms
                             source.text = currentcy.source
                             usdvnd.text = currentcy.quotes.USDVND.toString()
                         }
                     }
+                    Log.e("TAG", "onResponse: $currentcy")
                 }
 
                 override fun onFailure(call: Call<Currentcy>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "Call failed", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+    }
+
+    private fun startPost() {
+        val post = Post(Random.nextInt(1, 1000), 1, "Duc", "Ok")
+        ApiServices.apiServicesPost.sendPost(post)
+            .enqueue(object : Callback<Post> {
+                override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                    Toast.makeText(this@MainActivity, "Call Ok", Toast.LENGTH_SHORT).show()
+                    val postRes: Post? = response.body()
+                    postRes?.let {
+                        postResult.text = postRes.toString()
+                    }
+                }
+
+                override fun onFailure(call: Call<Post>, t: Throwable) {
                     Toast.makeText(this@MainActivity, "Call failed", Toast.LENGTH_SHORT).show()
                 }
 
--- Retrofit/app/src/main/java/com/example/retrofit/api/ApiServices.kt
+++ Retrofit/app/src/main/java/com/example/retrofit/api/ApiServices.kt
@@ -1,17 +1,19 @@
 package com.example.retrofit.api
 
 import com.example.retrofit.model.Currentcy
+import com.example.retrofit.model.Post
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ApiServices {
-    //link api:  http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
-    companion object{
+    //link api get:  http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
+    // link api post: https://jsonplaceholder.typicode.com/posts
+
+    companion object {
         private val gson: Gson
             get() = GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss")
                 .create()
@@ -19,6 +21,13 @@ interface ApiServices {
         val apiServices: ApiServices
             get() = Retrofit.Builder()
                 .baseUrl("http://apilayer.net/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(ApiServices::class.java)
+
+        val apiServicesPost: ApiServices
+            get() = Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
                 .create(ApiServices::class.java)
@@ -33,5 +42,17 @@ interface ApiServices {
         @Query("currencies") currencies: String,
         @Query("source") source: String,
         @Query("format") format: Int,
-        ): Call<Currentcy>
+    ): Call<Currentcy>
+
+    /* get with Map */
+    @GET("api/live")
+    fun convertUusToVndMap(@QueryMap value: Map<String, String>): Call<Currentcy>
+
+    /* get with full url */
+    @GET("api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1")
+    fun convertUusToVnd2(): Call<Currentcy>
+
+    /* POST */
+    @POST("posts")
+    fun sendPost(@Body post: Post): Call<Post>
 }
