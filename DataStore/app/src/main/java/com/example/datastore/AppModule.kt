7c37efdd36cfc1ab0b03119e1b083292fc0fb7e1
Đặng Đức <39004625+DangDuc1999@users.noreply.github.com>
2021-07-10 11:30:02 +0700

    add injection

--- DataStore/app/src/main/java/com/example/datastore/AppModule.kt
+++ DataStore/app/src/main/java/com/example/datastore/AppModule.kt
@@ -5,13 +5,20 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponentManager::class)
+@InstallIn(ActivityComponent::class)
 object AppModule {
 
     @Singleton
     @Provides
-    fun provideTestString() = "This is my string"
+    @Named("String1")
+    fun provideTestString1() = "This is my string"
+
+    @Singleton
+    @Provides
+    @Named("String2")
+    fun provideTestString2() = "This is my string"
 }
--- MyRoom/app/src/main/java/com/example/myroom/activitymain/MainActivity.kt
+++ MyRoom/app/src/main/java/com/example/myroom/activitymain/MainActivity.kt
@@ -47,6 +47,7 @@ class MainActivity : AppCompatActivity(), IPermissionRequest {
     private lateinit var accountDialog: AccountDialog
     /* ****** */
 
+    // test git squash 1
     /* interface */
 
     /* ************************** */
