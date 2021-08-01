35fc3e944db0b5581bf123fd2f5e42519167d097
Đặng Đức <39004625+DangDuc1999@users.noreply.github.com>
2021-08-01 19:24:42 +0700

    Init

---
+++ MVVMBeginner/app/src/main/java/com/example/mvvmbeginner/ui/main/view/activity/mainactivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmbeginner.ui.main.view.activity.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmbeginner.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
