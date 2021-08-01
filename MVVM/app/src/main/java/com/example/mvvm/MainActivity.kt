15aab38fa5f029da7d8bd51d488d057d3968b8b9
Đặng Đức <39004625+DangDuc1999@users.noreply.github.com>
2021-08-01 18:35:36 +0700

    init new

--- MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
+++ MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.example.mvvm
 
-import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mvvm.model.UserViewModel
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val model = UserViewModel()
-        model.getNum().observe(this, { num ->
-            // update UI
-            Toast.makeText(this, "Up $num", Toast.LENGTH_SHORT).show()
-        })
     }
 }
