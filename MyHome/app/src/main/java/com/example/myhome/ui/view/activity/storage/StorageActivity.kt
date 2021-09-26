a26bd29a6414d2e5740b3fc4a8ef6ac0ce853490
ducdang <ducdang513@gmail.com>
2021-09-26 22:17:35 +0700

    Change storage user nav graph to viewpager 2

--- MyHome/app/src/main/java/com/example/myhome/ui/view/activity/storage/StorageActivity.kt
+++ MyHome/app/src/main/java/com/example/myhome/ui/view/activity/storage/StorageActivity.kt
@@ -1,22 +1,18 @@
 package com.example.myhome.ui.view.activity.storage
 
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.myhome.BaseActivity
 import com.example.myhome.R
+import com.example.myhome.ui.adapter.storage.StorageVewPagerAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class StorageActivity : BaseActivity() {
 
-    private val navHostFragment: NavHostFragment by lazy {
-        supportFragmentManager.findFragmentById(R.id.nav_host_storage) as NavHostFragment
-    }
-
-    private lateinit var navController: NavController
+    private lateinit var mViewPager: ViewPager2
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var mStorageViewPagerAdapter: StorageVewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +22,44 @@ class StorageActivity : BaseActivity() {
 
         initVar()
 
-        bottomNavigationView.setupWithNavController(navController)
+        initLogicViewPager()
 
     }
 
     private fun initVar() {
-        navController = navHostFragment.navController
+        mStorageViewPagerAdapter = StorageVewPagerAdapter(this)
+        mViewPager = findViewById(R.id.vpg_storage)
         bottomNavigationView = findViewById(R.id.bottom_nav_storage)
+
+    }
+
+    private fun initLogicViewPager() {
+        mViewPager.adapter = mStorageViewPagerAdapter
+        mViewPager.offscreenPageLimit = 3
+
+        mViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when (position) {
+                    0 -> bottomNavigationView.menu
+                        .findItem(R.id.imageStorageFragment).isChecked = true
+                    1 -> bottomNavigationView.menu
+                        .findItem(R.id.videoStorageFragment).isChecked = true
+                    2 -> bottomNavigationView.menu
+                        .findItem(R.id.pdfStorageFragment).isChecked = true
+                }
+            }
+        })
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.imageStorageFragment -> mViewPager.currentItem = 0
+                R.id.videoStorageFragment -> mViewPager.currentItem = 1
+                R.id.pdfStorageFragment -> mViewPager.currentItem = 2
+            }
+
+            true
+        }
     }
 
 }
