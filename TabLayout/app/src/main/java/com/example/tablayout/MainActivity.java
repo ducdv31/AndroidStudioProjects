f944a392d27a45df5d5336c26454fb53f25baf31
ducdang <ducdang513@gmail.com>
2021-11-14 13:17:10 +0700

    Tab Layout compose ok

--- TabLayout/app/src/main/java/com/example/tablayout/MainActivity.java
+++ TabLayout/app/src/main/java/com/example/tablayout/MainActivity.java
@@ -25,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
---
+++ TabLayoutCompose/app/src/main/java/com/example/tablayoutcompose/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.tablayoutcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tablayoutcompose.ui.theme.TabLayoutComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TabLayoutComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewApp() {
+    MyApp()
+}
+
+@Composable
+fun MyApp() {
+    Scaffold() {
+        Column {
+            Tab(selected = true, onClick = { }) {
+                Text(text = "Tab 1")
+                Text(text = "Tab 1.2")
+            }
+            Tab(selected = false, onClick = { /*TODO*/ }) {
+                Text(text = "Tab 2")
+            }
+
+            /* Tab row */
+            var tabIndex by remember { mutableStateOf(0) }
+            val tabData = listOf(
+                "MUSIC" to Icons.Filled.Home,
+                "MARKET" to Icons.Filled.ShoppingCart,
+                "FILMS" to Icons.Filled.AccountBox,
+                "BOOKS" to Icons.Filled.Settings,
+            )
+
+            ScrollableTabRow(
+                selectedTabIndex = tabIndex,
+                divider = {
+                    TabRowDefaults.Divider(
+                        thickness = 2.dp,
+                        color = Color.Red
+                    )
+                },
+                indicator = {
+                    TabRowDefaults.Indicator(
+                        modifier = Modifier.tabIndicatorOffset(it[tabIndex]),
+                        height = 5.dp,
+                        color = Color.Green
+                    )
+                },
+                edgePadding = 16.dp
+            ) {
+                tabData.forEachIndexed { index, pair ->
+                    Tab(selected = tabIndex == index, onClick = {
+                        tabIndex = index
+                    }, text = {
+                        Text(
+                            text = pair.first,
+                            fontWeight = if (tabIndex == index) FontWeight.Bold else FontWeight.Normal
+                        )
+                    }, icon = {
+                        Icon(imageVector = pair.second, contentDescription = null)
+                    })
+                }
+            }
+
+            val modifierText = Modifier.align(alignment = CenterHorizontally)
+            val modifierImage = Modifier
+                .size(50.dp)
+                .fillMaxWidth()
+                .align(alignment = CenterHorizontally)
+
+            when (tabIndex) {
+                0 -> {
+                    Text(
+                        text = "Tab 1",
+                        modifier = modifierText
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.outline_home_black_48dp),
+                        contentDescription = null,
+                        modifier = modifierImage
+                    )
+                }
+                1 -> {
+                    Text(
+                        text = "Tab 2",
+                        modifier = modifierText
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.outline_local_grocery_store_black_24dp),
+                        contentDescription = null,
+                        modifier = modifierImage
+                    )
+                }
+                2 -> {
+                    Text(
+                        text = "Tab 3",
+                        modifierText
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.outline_confirmation_number_black_24dp),
+                        contentDescription = null,
+                        modifier = modifierImage
+                    )
+                }
+                3 -> {
+                    Text(
+                        text = "Tab 4",
+                        modifierText
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.outline_menu_book_black_24dp),
+                        contentDescription = null,
+                        modifier = modifierImage
+                    )
+                }
+            }
+        }
+    }
+}
