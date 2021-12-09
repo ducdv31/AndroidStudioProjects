482e052584cf26752c3d67c31b8ba4f81909a478
ducdang <ducdang513@gmail.com>
2021-12-09 14:32:21 +0700

    Refactoring code Flutter recipe

--- recipe_flutter/lib/main.dart
+++ recipe_flutter/lib/main.dart
@@ -6,36 +6,22 @@ import 'package:flutter/rendering.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:recipe_flutter/net/api/recipe/api_client_recipe.dart';
 import 'package:recipe_flutter/net/api/recipe/model/recipe_model.dart';
+import 'package:recipe_flutter/ui/recipe_screen/recipe_screen.dart';
 
 import 'constant/constant.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _StateMyApp();
 }
 
 class _StateMyApp extends State<MyApp> {
-  final dio = Dio();
-  var page = 1;
-  var query = EMPTY;
-
-  final RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    /* get data recipe */
-    dio.options.contentType = CONTENT_TYPE;
-    dio.options.headers[HEADER_KEY] = HEADER_VALUE;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,109 +31,6 @@ class _StateMyApp extends State<MyApp> {
           primarySwatch: Colors.blue,
         ),
         home: SafeArea(
-            child: Scaffold(
-                resizeToAvoidBottomInset: false,
-                appBar: AppBar(
-                  title: const Text("Recipe"),
-                  centerTitle: true,
-                ),
-                drawer: const Text("Drawer"),
-                body: listRecipe())));
-  }
-
-  StreamBuilder<ResponseRecipe> streamRecipe() {
-    return StreamBuilder<ResponseRecipe>(
-      stream: null,
-      builder: (context, snapshot) {
-        final list = snapshot.data?.results;
-        return ListView.builder(
-            itemCount: list?.length,
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(10),
-            itemBuilder: (context, index) => Card(
-                  elevation: 8,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Padding(padding: EdgeInsets.all(2)),
-                        Container(
-                          child: ClipRRect(
-                            child: Image.network(
-                              list?.elementAt(index).featuredImage ?? EMPTY,
-                              fit: BoxFit.fitWidth,
-                              height: 220,
-                            ),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                        ),
-                        Container(
-                          child: Text(
-                            list?.elementAt(index).title ?? EMPTY,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 8),
-                        ),
-                      ]),
-                ));
-      },
-    );
-  }
-
-  FutureBuilder<ResponseRecipe> listRecipe() {
-    return FutureBuilder<ResponseRecipe>(
-      future: ApiClientRecipe(dio).getListRecipe(page, query),
-      builder: (context, snapshot) {
-        var list = snapshot.data?.results;
-        return ListView.builder(
-            itemCount: list?.length,
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(10),
-            itemBuilder: (context, index) => Card(
-                  elevation: 8,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Padding(padding: EdgeInsets.all(2)),
-                        Container(
-                          child: ClipRRect(
-                            child: Image.network(
-                              list?.elementAt(index).featuredImage ?? EMPTY,
-                              fit: BoxFit.fitWidth,
-                              height: 220,
-                            ),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                        ),
-                        Container(
-                          child: Text(
-                            list?.elementAt(index).title ?? EMPTY,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 8),
-                        ),
-                      ]),
-                ));
-      },
-    );
+            child: recipeScreen()));
   }
 }
---
+++ recipe_flutter/lib/ui/recipe_screen/recipe_screen.dart
@@ -0,0 +1,122 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_flutter/constant/constant.dart';
+import 'package:recipe_flutter/net/api/recipe/api_client_recipe.dart';
+import 'package:recipe_flutter/net/api/recipe/model/recipe_model.dart';
+
+final dio = Dio();
+var page = 1;
+var query = EMPTY;
+
+Scaffold recipeScreen() {
+
+  /* get data recipe */
+  dio.options.contentType = CONTENT_TYPE;
+  dio.options.headers[HEADER_KEY] = HEADER_VALUE;
+
+  return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text("Recipe"),
+        centerTitle: true,
+      ),
+      drawer: const Text("Drawer"),
+      body: listRecipe());
+}
+
+StreamBuilder<ResponseRecipe> streamRecipe() {
+  return StreamBuilder<ResponseRecipe>(
+    stream: null,
+    builder: (context, snapshot) {
+      final list = snapshot.data?.results;
+      return ListView.builder(
+          itemCount: list?.length,
+          physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(10),
+          itemBuilder: (context, index) => Card(
+            elevation: 8,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Padding(padding: EdgeInsets.all(2)),
+                  Container(
+                    child: ClipRRect(
+                      child: Image.network(
+                        list?.elementAt(index).featuredImage ?? EMPTY,
+                        fit: BoxFit.fitWidth,
+                        height: 220,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                  ),
+                  Container(
+                    child: Text(
+                      list?.elementAt(index).title ?? EMPTY,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8, vertical: 8),
+                  ),
+                ]),
+          ));
+    },
+  );
+}
+
+FutureBuilder<ResponseRecipe> listRecipe() {
+  return FutureBuilder<ResponseRecipe>(
+    future: ApiClientRecipe(dio).getListRecipe(page, query),
+    builder: (context, snapshot) {
+      var list = snapshot.data?.results;
+      return ListView.builder(
+          itemCount: list?.length,
+          physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(10),
+          itemBuilder: (context, index) => Card(
+            elevation: 8,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Padding(padding: EdgeInsets.all(2)),
+                  Container(
+                    child: ClipRRect(
+                      child: Image.network(
+                        list?.elementAt(index).featuredImage ?? EMPTY,
+                        fit: BoxFit.fitWidth,
+                        height: 220,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                  ),
+                  Container(
+                    child: Text(
+                      list?.elementAt(index).title ?? EMPTY,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8, vertical: 8),
+                  ),
+                ]),
+          ));
+    },
+  );
+}
