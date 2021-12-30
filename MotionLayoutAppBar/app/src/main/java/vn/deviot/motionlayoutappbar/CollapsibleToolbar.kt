e1d997c430f85f621608996a237c3b7fe329cb09
ducdang <ducdang513@gmail.com>
2021-12-31 01:06:31 +0700

    Shapeable ImageView

--- MotionLayoutAppBar/app/src/main/java/vn/deviot/motionlayoutappbar/CollapsibleToolbar.kt
+++ MotionLayoutAppBar/app/src/main/java/vn/deviot/motionlayoutappbar/CollapsibleToolbar.kt
@@ -18,12 +18,15 @@ package vn.deviot.motionlayoutappbar
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.isVisible
 import com.google.android.material.appbar.AppBarLayout
 
 class CollapsibleToolbar @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : MotionLayout(context, attrs, defStyleAttr), AppBarLayout.OnOffsetChangedListener {
+    private val TAG = CollapsibleToolbar::class.java.simpleName
 
     override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
         progress = -verticalOffset / appBarLayout?.totalScrollRange?.toFloat()!!
