a6704e70ab755748016d3939249521ba89643ea6
Đặng Đức <ducdang513@gmail.com>
2022-02-06 14:38:04 +0700

    UI Subscribe screen

--- home/lib/screen/mqtt/subscribe/subscribe_screen.dart
+++ home/lib/screen/mqtt/subscribe/subscribe_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:home/component/textfield_component.dart';
+import 'package:home/mqtt/mqtt_config.dart';
 
 class SubscribeScreen extends StatefulWidget {
   const SubscribeScreen({Key? key}) : super(key: key);
@@ -8,10 +11,35 @@ class SubscribeScreen extends StatefulWidget {
 }
 
 class _SubscribeScreenState extends State<SubscribeScreen> {
+  String topic = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Subscribe"),
+    return Center(
+      child: Expanded(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: textFieldCommon(
+                "Topic",
+                (text) {
+                  topic = text;
+                },
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () async {
+                final MqttConfig _mqttConfig = MqttConfig();
+                await _mqttConfig.connectMqtt();
+                _mqttConfig.subscribe(topic);
+                _mqttConfig.listenUpdate();
+              },
+              child: const Text("Subscribe"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
