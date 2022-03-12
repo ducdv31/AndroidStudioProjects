e5238117c55ed8cd2129d601d1dfdce77530d284
Đặng Đức <ducdang513@gmail.com>
2022-03-12 18:03:36 +0700

    Init project My Video

---
+++ MyVideo/app/src/main/java/vn/dv/myvideo/MainActivity.kt
@@ -0,0 +1,13 @@
+package vn.dv.myvideo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
